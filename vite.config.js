730caeb637420e9ad19ecf72fe586e6db90f0b96
Sachin <sahanisachin60@gmail.com>
2026-08-12 08:55:00 +0530

    Add some changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    minify: true,
+    cssMinify: true,
+    cssCodeSplit: true,
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
